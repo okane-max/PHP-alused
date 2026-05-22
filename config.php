2a1d73320065eff94b6f4508ca2597371321e039
okane-max <okane@hkhk.edu.ee>
2026-05-22 09:25:24 +0300

    changed database info

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
     // Sinu andmed
-    $db_server = 'localhost';
+    $db_server = 'db';
     $db_andmebaas = 'autorent';
-    $db_kasutaja = 'mario';
-    $db_salasona = 'mario';
+    $db_kasutaja = 'okane';
+    $db_salasona = 'okane';
 
     // Ühendus andmebaasiga
     $yhendus = mysqli_connect($db_server, $db_kasutaja, $db_salasona, $db_andmebaas);
